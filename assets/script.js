9cfe6fb73a07a81daa7c2cee50fe21d655abc042
Xart94 <fayz_mohamed2@hotmail.fr>
2024-05-02 13:20:52 +0200

    updated html & added event listeners on arrows

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,29 @@
+const flecheDroite = document.querySelector(".arrow_right");
+const flecheGauche = document.querySelector(".arrow_left");
+
 const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-]
+  {
+    image: "slide1.jpg",
+    tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
+  },
+  {
+    image: "slide2.jpg",
+    tagLine:
+      "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+  },
+  {
+    image: "slide3.jpg",
+    tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+  },
+  {
+    image: "slide4.png",
+    tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+  },
+];
+
+flecheDroite.addEventListener("click", () => {
+  console.log("click droite");
+});
+flecheGauche.addEventListener("click", () => {
+  console.log("click gauche");
+});
